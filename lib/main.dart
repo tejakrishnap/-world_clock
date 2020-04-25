fa39ba0b5dc1dd196af311b048947763330de99d
Teja Krishna <saikrishnateja.palepu@sprint.com>
2020-04-25 18:57:32 -0500

    Added navigation from home screen to location screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:worldclock/pages/choose_location.dart';
 import 'package:worldclock/pages/home.dart';
+import 'package:worldclock/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/home', // initial route will be the starting screen of the app
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation()
+  },
 ));
 
